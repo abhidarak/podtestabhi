b4f224cb9a753c432ebc96d20bbdfbb4a29f8764
abhishekdarak <97934871+abhishekdarak@users.noreply.github.com>
2022-12-05 14:11:21 +0530

    testing 2

--- api.swift
+++
@@ -1,5 +0,0 @@
-class api {
-
-var test
-
-}
---
+++ testsdk/testsdk/testsdk.h
@@ -0,0 +1,18 @@
+//
+//  testsdk.h
+//  testsdk
+//
+//  Created by Abhishek Darak on 05/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testsdk.
+FOUNDATION_EXPORT double testsdkVersionNumber;
+
+//! Project version string for testsdk.
+FOUNDATION_EXPORT const unsigned char testsdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testsdk/PublicHeader.h>
+
+
